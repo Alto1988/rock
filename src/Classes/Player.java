9d16c3b4f0109bd0a7f6c602a6751b929c64b0f0
Jacob Pagan <jake2415@gmail.com>
2021-12-09 19:06:31 -0600

    Getting things to work for the updating of the players score

--- src/Classes/Player.java
+++ src/Classes/Player.java
@@ -1,6 +1,26 @@
 package Classes;
 
-public class Player {
-    private int score;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Player {
+    private int _score;
+    private final List<String> _results = new ArrayList<>();
+
+    public Player(){
+        _score = 0;
+    }
+    public void setWins(String result){
+        //Basically everytime a player wins, or loses, the score is updated
+        _results.add(result);
+    }
+    public void updateScore(){
+       _score++;
+    }
+    public int getScore(){
+        return _score;
+    }
+    public String getName(){
+        return this.getClass().getSimpleName();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import Classes.Player;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -9,14 +13,55 @@ public class Main {
         history,
         quit
     }
-    public static void gameRockPaperScissorsLogic(String playerOneChoice, String playerTwoChoice){
+    public static ArrayList<String> Choices = new ArrayList<String>(Arrays.asList("rock", "paper", "scissors"));
+    public static void gameRockPaperScissorsLogic(Player playerOne, Player playerTwo, String playerOneChoice, String playerTwoChoice){
         //Inide here we find the winner and update that player's score
+        System.out.println("Player One chose " + playerOneChoice);
+        System.out.println("Player Two chose " + playerTwoChoice);
+        switch(playerOneChoice){
+            case "rock":
+                if(playerTwoChoice.equals("rock"))
+                    break;
+                else if(playerTwoChoice.equals("paper")){
+                    playerTwo.updateScore();
+                    break;
+                }else {
+                    playerOne.updateScore();
+                }
+                break;
+                case "paper":
+                    if(playerTwoChoice.equals("paper"))break;
+                    else if (playerTwoChoice.equals("scissors")){
+                        playerTwo.updateScore();
+                        break;
+                    }else {
+                        playerOne.updateScore();
+                    }
+                    break;
+                    case "scissors":
+                        if(playerTwoChoice.equals("scissors"))break;
+                        else if(playerTwoChoice.equals("rock")){
+                            playerTwo.updateScore();
+                            break;
+                        }else {
+                            playerOne.updateScore();
+                            break;
+                        }
+
+        }
+
+        System.out.println("Player One score is " + playerOne.getScore());
+        System.out.println("Player Two score is " + playerTwo.getScore());
     }
 
 
     public static void main(String[] args) {
         boolean gameOver = false;
         boolean choiceIsValid = false;
+
+        Player playerOne = new Player();
+        Player playerTwo = new Player();
+
 
         Scanner howManyPlayers = new Scanner(System.in);
         while(!choiceIsValid){
@@ -37,17 +82,23 @@ public class Main {
                     String startMenuChoice = playerScanners.nextLine();
                     if(startMenuChoice.equals(MenuChoices.play.toString())){
                         //We start the game
+                        Scanner scanner = new Scanner(System.in);
+                        System.out.println("Player 1, please enter your choice: ");
+                        String playerOneChoice = scanner.nextLine();
+                        if(Choices.contains(playerOneChoice)){
+                            System.out.println("!!!!!!ENTER A VALID CHOICE!!!!!!");
+                        }
+                        System.out.println("Player 2, please enter your choice: ");
+                        String playerTwoChoice = scanner.nextLine();
+                        if(Choices.contains(playerTwoChoice)){
+                            System.out.println("!!!!!!ENTER A VALID CHOICE!!!!!!");
+                        }
+                        gameRockPaperScissorsLogic(playerOne, playerTwo, playerOneChoice, playerTwoChoice);
                     }else if(startMenuChoice.equals(MenuChoices.history.toString())){
                         //display the history of the game
                     }else if(startMenuChoice.equals(MenuChoices.quit.toString())){
                         gameOver = true;
                     }
-//            Scanner scanner = new Scanner(System.in);
-//            System.out.println("Player 1, please enter your choice: ");
-//            String playerOneChoice = scanner.nextLine();
-//            System.out.println("Player 2, please enter your choice: ");
-//            String playerTwoChoice = scanner.nextLine();
-//            gameRockPaperScissorsLogic(playerOneChoice, playerTwoChoice);
                 }
             }else if(numberOfPlayers == 2){
                 choiceIsValid = true;
@@ -63,6 +114,18 @@ public class Main {
                     Scanner playerScanners = new Scanner(System.in);
                     String startMenuChoice = playerScanners.nextLine();
                     if(startMenuChoice.equals(MenuChoices.play.toString())){
+                        Scanner scanner = new Scanner(System.in);
+                        System.out.println("Player 1, please enter your choice: ");
+                        String playerOneChoice = scanner.nextLine();
+                        if(!Choices.contains(playerOneChoice)){
+                            System.out.println("!!!!!!ENTER A VALID CHOICE!!!!!!");
+                        }
+                        System.out.println("Player 2, please enter your choice: ");
+                        String playerTwoChoice = scanner.nextLine();
+                        if(!Choices.contains(playerTwoChoice)){
+                            System.out.println("!!!!!!ENTER A VALID CHOICE!!!!!!");
+                        }
+                        gameRockPaperScissorsLogic(playerOne, playerTwo, playerOneChoice, playerTwoChoice);
                         //We start the game
                     }else if(startMenuChoice.equals(MenuChoices.history.toString())){
                         //display the history of the game
